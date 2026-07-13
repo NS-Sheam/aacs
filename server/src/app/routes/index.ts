97c657a835f16b7cc0fda07e1462e80324bd6cc6
NS-Sheam <123sheamfeni@gmail.com>
2026-07-13 12:08:33 +0600

    add: submission module

--- server/src/app/routes/index.ts
+++ server/src/app/routes/index.ts
@@ -2,7 +2,7 @@ import { Router } from "express";
 import { assignmentsRoutes } from "../modules/assignment/assignments.routes";
 import { resultsRoutes } from "../modules/result/results.routes";
 import { reviewQueueRoutes } from "../modules/reviewQueue/reviewQueue.routes";
-import { submissionsRoutes } from "../modules/submission/submissions.routes";
+import { SubmissionsRoutes } from "../modules/submission/submissions.routes";
 
 const router = Router();
 
@@ -11,7 +11,7 @@ const apiRoutes = [
   { path: "/assignments", route: assignmentsRoutes },
   { path: "/results", route: resultsRoutes },
   { path: "/review-queue", route: reviewQueueRoutes },
-  { path: "/submissions", route: submissionsRoutes },
+  { path: "/submissions", route: SubmissionsRoutes },
 ] as Record<string, any>[];
 
 apiRoutes.forEach((route) => router.use(route.path, route.route));
